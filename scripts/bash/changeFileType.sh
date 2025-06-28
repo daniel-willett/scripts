c9cf8671a74b19bfb8e5e4f0f050da3e6680d1f4
daniel-willett <willettdr1@gmail.com>
2025-06-28 11:10:51 +0100

    Added functionality for apending extensions to files with no extention type Additionally fixed spelling of "extention" to "extension" within all text and variable names

--- scripts/bash/changeFileType.sh
+++ scripts/bash/changeFileType.sh
@@ -1,23 +1,33 @@
 #!/usr/bin/env bash
 
-startExtention=$1
-endExtention=$2
+startExtension=$1
+endExtension=$2
 
-if [ "$startExtention" == "-h" ] || [ "$startExtention" == "--help" ]; then
+if [ "$startExtension" == "-h" ] || [ "$startExtension" == "--help" ]; then
 	echo "$(changeFileType-help)"
 	exit 1
-elif [ "$startExtention" == "-s" ] || [ "$startExtention" == "--strip" ]; then
-	startExtention=$endExtention
-	for file in *."$startExtention" ; do
-		mv -- "$file" "${file%.$startExtention}"
+elif [ "$startExtension" == "-s" ] || [ "$startExtension" == "--strip" ]; then
+	startExtension=$endExtension
+	for file in *."$startExtension" ; do
+		mv -- "$file" "${file%.$startExtension}"
 	done
-	echo "Stripped all files within this directory with extention $startExtention to no extention"
+	echo "Stripped all files within this directory with extension $startExtension to no extension"
 	exit 1
+elif [ "$startExtension" == "-a" ] || [ "$startExtension" == "--add" ]; then
+	for file in * ; do
+		if [[ -f "$file" && "$file" != *.* ]]; then
+			mv -- "$file" "$file.$endExtension"
+		fi
+	done
+	echo "Added all files within this directory with extension no extension to $startExtension extension"
+	exit 1
+
+
 fi
 
 
-for file in *."$startExtention" ; do 
-	mv -- "$file" "${file%.$startExtention}.$endExtention"
+for file in *."$startExtension" ; do 
+	mv -- "$file" "${file%.$startExtension}.$endExtension"
 done
-echo "Moved all files within this directory with extention $startExtention to $endExtention"
+echo "Moved all files within this directory with extension $startExtension to $endExtension"
 exit 1
