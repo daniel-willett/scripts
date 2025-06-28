c0c84846b9d969ee93926b77e83a01b46537c888
daniel-willett <willettdr1@gmail.com>
2025-06-28 09:53:19 +0100

    Added functionality to strip files of their filetype. e.g. `./changeFileType.sh -s php` would perform `test.php` -> `test`

--- scripts/bash/changeFileType.sh
+++ scripts/bash/changeFileType.sh
@@ -5,8 +5,19 @@ endExtention=$2
 
 if [ "$startExtention" == "-h" ] || [ "$startExtention" == "--help" ]; then
 	echo "$(changeFileType-help)"
-else
-	for file in *."$startExtention" ; do 
-		mv -- "$file" "${file%.$startExtention}.$endExtention"
+	exit 1
+elif [ "$startExtention" == "-s" ] || [ "$startExtention" == "--strip" ]; then
+	startExtention=$endExtention
+	for file in *."$startExtention" ; do
+		mv -- "$file" "${file%.$startExtention}"
 	done
+	echo "Stripped all files within this directory with extention $startExtention to no extention"
+	exit 1
 fi
+
+
+for file in *."$startExtention" ; do 
+	mv -- "$file" "${file%.$startExtention}.$endExtention"
+done
+echo "Moved all files within this directory with extention $startExtention to $endExtention"
+exit 1
