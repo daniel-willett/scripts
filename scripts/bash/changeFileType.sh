1fac632d4ff159327d31a861cb5f0c8b4788de78
daniel-willett <willettdr1@gmail.com>
2025-09-08 21:19:11 +0100

    Initial start at tidying up code. Future idea is to have flags like `-r/--recursive` which can be intertwined with the other flags meaning need a better way to process flags. Seems like `getopt` is the way

--- scripts/bash/changeFileType.sh
+++ scripts/bash/changeFileType.sh
@@ -1,5 +1,64 @@
 #!/usr/bin/env bash
 
+
+## https://stackoverflow.com/questions/192249/how-do-i-parse-command-line-arguments-in-bash?page=1&tab=scoredesc#tab-top
+
+
+#options --longer/-l
+#requires at least 1 argument
+LONGOPTS=help,strip,:add
+OPTIONS=hsa
+
+#We store the output
+#Activate quoting/enhanced mode by "--options"
+#pass arguments only via   -- "$@"   to separate them correctly
+#if getopt fails, it complains itself ot stderr
+PARSED=$(getopt --options=$OPTIONS --longoptions=$LONGOPTS --name "$0" -- "$@") || exit 2
+
+#read getopt's output this way to handle quoting right:
+eval set -- "$PARSED"
+
+#initialise
+h=false s=false a=false
+
+while true; do
+	case "$1" in
+		-h|--help)
+			h=true
+			shift
+			;;
+		-s|--strip)
+			s=true
+			shift
+			;;
+		-a|--add)
+			a=true
+			shift
+			;;
+		--)
+			shift
+			break
+			;;
+		*)
+			echo "Error"
+			exit 3
+			;;
+	esac
+done
+
+#handle non-option arguments
+if [[ $# -ne 1]]; then
+	echo "$0: a single input file is required."
+	exit 4
+fi
+
+echo "verbose: $v, force: $f, debug: $d, in: $1, out $2"
+
+#
+#=======================
+#
+#=======================
+
 startExtension=$1
 endExtension=$2
 
